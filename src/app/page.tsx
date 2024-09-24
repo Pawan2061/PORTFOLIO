2647fb1a4e95a84333d4761a0e4176258300000e
Pawan <pandeypawan2061@gmail.com>
2024-09-25 01:15:45 +0530

    feat: finished ui section of aboutme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,80 +14,74 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <main className="min-h-screen relative inset-0 rounded-full flex flex-col items-center mt-32 space-y-8 max-w-2xl mx-auto p-2  ">
-      <div className="flex justify-between items-center">
-        <div className="flex-1 space-y-5">
-          <h1 className="font-bold">
-            Pawan Pandey{" "}
-            <span className="text-xs from-neutral-900 bg-gray-400 rounded-xl p-1">
-              <a
-                href="mailto:pandeypawan2061@gmail.com"
+      <main className="min-h-screen relative inset-0 rounded-full flex flex-col items-center mt-32 space-y-8 max-w-2xl mx-auto p-2  ">
+        <div className="flex justify-between items-center">
+          <div className="flex-1 space-y-5">
+            <h1 className="font-bold">
+              Pawan Pandey{" "}
+              <span className="text-xs from-neutral-900 bg-gray-400 rounded-xl p-1">
+                <a
+                  href="mailto:pandeypawan2061@gmail.com"
+                  className="cursor-pointer"
+                >
+                  Let&apos;s contact
+                </a>
+              </span>
+            </h1>
+            <p className="max-w-sm text-pretty font-mono text-sm">
+              Full-stack web developer exploring new opportunities and excited
+              to work on exciting gigs.
+            </p>
+
+            <div className="flex space-x-2">
+              <Link
+                href="https://github.com"
+                target="_blank"
                 className="cursor-pointer"
               >
-                Let&apos;s contact
-              </a>
-            </span>
-          </h1>
-          <p className="max-w-sm text-pretty font-mono text-sm">
-            Full-stack web developer exploring new opportunities and excited to
-            work on exciting gigs.
-          </p>
+                <span className="hover:text-gray-500">
+                  <GitHubLogoIcon />
+                </span>
+              </Link>
+              <Link
+                href="https://instagram.com"
+                target="_blank"
+                className="cursor-pointer"
+              >
+                <span className="hover:text-gray-500">
+                  <InstagramLogoIcon />
+                </span>
+              </Link>
+              <Link
+                href="https://twitter.com"
+                target="_blank"
+                className="cursor-pointer"
+              >
+                <span className="hover:text-gray-500">
+                  <TwitterLogoIcon />
+                </span>
+              </Link>
+            </div>
+          </div>
 
-          <div className="flex space-x-2">
-            <Link href="https://github.com" target="_blank" className="cursor-pointer">
-              <span className="hover:text-gray-500">
-                <GitHubLogoIcon />
-              </span>
-            </Link>
-            <Link href="https://instagram.com" target="_blank" className="cursor-pointer">
-              <span className="hover:text-gray-500">
-                <InstagramLogoIcon />
-              </span>
-            </Link>
-            <Link href="https://twitter.com" target="_blank" className="cursor-pointer">
-              <span className="hover:text-gray-500">
-                <TwitterLogoIcon />
-              </span>
-            </Link>
+          <div>
+            <Image
+              src="https://pbs.twimg.com/profile_images/1750126817590087680/7AydU7yH_400x400.jpg"
+              alt="Profile Picture"
+              width={112}
+              height={112}
+              className="relative flex shrink-0 overflow-hidden rounded-xl size-28"
+            />
           </div>
         </div>
 
-        <div>
-          <Image
-            src="https://pbs.twimg.com/profile_images/1750126817590087680/7AydU7yH_400x400.jpg"
-            alt="Profile Picture"
-            width={112}
-            height={112}
-            className="relative flex shrink-0 overflow-hidden rounded-xl size-28"
-          />
-        </div>
-      </div>
-      
-      
+        <WorkExperience />
+        <ProjectCards />
 
+        <Skill />
 
-        <WorkExperience/>
-        <ProjectCards/>
-
-      
-          <Skill/>
-
-          <AboutMe/>
-
-          
-      
-       
-     
-
-     
-     
-     
-    </main>
-   
-
-
+        <AboutMe />
+      </main>
     </>
-
-   
   );
 }
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,24 @@
-export default function AboutMe(){
-    return(
-        <div>its me</div>
-    )
+"use client";
+import { useEffect, useState } from "react";
+
+export default function AboutMe() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // Update the time every second
+
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, []);
+
+  return (
+    <article>
+      <div className="border-t w-[900px] border-dotted  border-gray-400 space-y-1"></div>
+      <div className="flex justify-around">
+        <h1 className="font-light text-sm  ">Crafted by Pawan Pandey</h1>
+        <h1>Banglore: {currentTime.toLocaleTimeString()}</h1>
+      </div>
+    </article>
+  );
 }
